a64ec419db6672997776aa04f4e87bab49ea72bb
yuta kadotamai <kadotami@users.noreply.github.com>
2019-07-24 09:47:07 +0900

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sassLint = require('gulp-sass-lint');
 var pug = require('gulp-pug');
 var connect = require('gulp-connect');
 
 gulp.task('connect', function(cb){
   connect.server({
-    root: 'public',
+    root: './',
     livereload: true
   });
   return cb();
@@ -16,30 +15,20 @@ gulp.task('connect', function(cb){
 gulp.task('sass', function () {
   return gulp.src('./scss/style.scss')
       .pipe(sass({outputStyle: 'expanded'}))
-      .pipe(gulp.dest('./public'));
-});
-
-gulp.task('sass-lint', function () {
-  return gulp.src('./scss/**/*.scss')
-      .pipe(sassLint())
-      .pipe(sassLint.format())
-      .pipe(sassLint.failOnError())
+      .pipe(gulp.dest('./'))
+      .pipe(connect.reload());
 });
 
 gulp.task('pug', () => {
   return gulp.src('./pug/index.pug')
     .pipe(pug({pretty: true}))
-    .pipe(gulp.dest('./public'));
-});
-
-gulp.task('livereload', function (){
-  return gulp.src('./public/**/*')
+    .pipe(gulp.dest('./'))
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', gulp.series('sass-lint', 'sass','livereload'));
-  gulp.watch('./pug/index.pug', gulp.series('pug','livereload'));
+  gulp.watch('./scss/**/*.scss',{interval: 500}, gulp.series('sass'));
+  gulp.watch('./pug/index.pug',{interval: 500}, gulp.series('pug'));
 });
 
 gulp.task('default', gulp.parallel('connect', 'watch'), function(){
